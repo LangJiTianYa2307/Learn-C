a4639fffbc6d3d8427eed423171a2698221130bd
huzhenglang <979972717@qq.com>
2020-05-03 11:26:01 +0800

    true code

--- reaarange.c
+++ reaarange.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "reaarange.h"
 
 //git remote add origin url
@@ -8,47 +8,65 @@
 //git push]
 //git commit -m "some information"
 
-int main(void){
+int main(void)
+{
     int n_columns;
     int columns[MAX_COLS];
-    n_columns = read_column_numbers(columns,MAX_COLS);
+    char input[MAX_INPUT];
+    char output[MAX_INPUT];
+    n_columns = read_column_numbers(columns, MAX_COLS);
+
+    //读取、处理和打印剩余的输入行
+    while (gets(input) != NULL)
+    {
+        printf("original input:%s\n", input);
+        rearrange(output, input, n_columns, columns);
+        printf("Rearranged line:%s\n", output);
+    }
+
     //printf("this is some problem to resolve.");
     return EXIT_SUCCESS;
 }
 
-int read_column_numbers(int columns[], int max){
-    int num =0;
+int read_column_numbers(int columns[], int max)
+{
+    int num = 0;
     int ch;
     /*
     **获取列标号，如果读取到的值小于0，则马上停止 
     */
-    while(num<max &&scanf("%d",&columns[num])&& columns[num]>=0){
+    while (num < max && scanf("%d", &columns[num]) && columns[num] >= 0)
+    {
         num++;
     }
 
-    if(num %2 ==0){
+    if (num % 2 != 0)
+    {
         puts("Last column number is not paired.");
         exit(EXIT_FAILURE);
     }
 
-    while((ch = getchar()) != EOF && ch != '\n');
+    while ((ch = getchar()) != EOF && ch != '\n')
+        ;
     return num;
 }
 
-void rearrange(char *output,char const *input,int n_columns,int const columns[]){
+void rearrange(char *output, char const *input, int n_columns, int const columns[])
+{
     int col;
     int len;
     int output_col;
-    int intput;
-    len = strlen(intput);
-    for(col=0;col<n_columns;col+=2){
-        int nchars = columns[col+1] - columns[col]+1;
-        if(columns[col] >= len || output_col == MAX_INPUT -1)
+    output_col=0;
+    len = (int)strlen(input);
+    for (col = 0; col < n_columns; col += 2)
+    {
+        int nchars = columns[col + 1] - columns[col] + 1;
+        if (columns[col] >= len || output_col == MAX_INPUT - 1)
             break;
-        if(output_col + nchars > MAX_INPUT -1)
-            nchars = MAX_INPUT - output_col -1;
+        if (output_col + nchars > MAX_INPUT - 1)
+            nchars = MAX_INPUT - output_col - 1;
         //复制数据
-        strncpy(output+output_col,intput+columns[col],nchars);
+        strncpy(output + output_col, input + columns[col], nchars);
         output_col += nchars;
     }
     output[output_col] = '\0';
--- reaarange.h
+++ reaarange.h
@@ -5,6 +5,6 @@
 #define MAX_INPUT 1000
 
 int read_column_numbers(int columns[], int max);
-void rearrange(char *output,char const *input,int n_columns,int const columns[]);
+void rearrange(char *output, char const *input, int n_columns, int const columns[]);
 
 #endif
